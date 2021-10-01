ecac524945a633c4877cef7a37804944a2753f4e
Brittney Braxton <brittneynbraxton@gmail.com>
2021-10-01 18:08:34 -0400

    Add styling for login

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			spacing: {
+				72: '18rem',
+				84: '21rem',
+				96: '24rem',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
