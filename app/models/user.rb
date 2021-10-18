28b621c5d9ea3e054af550645597529b6dc1c6c7
Brittney Braxton <brittneynbraxton@gmail.com>
2021-10-18 16:17:26 -0400

    Authenticate with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+  
+  attr_writer :login
+  validates_uniqueness_of :email
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_authenticatable(login)
+    where("username = :value OR email = :value", value: login).first
+  end
+  
+  def self.find_for_database_authentication(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    find_authenticatable(login)
+  end
+  
+  private
+    def ensure_proper_name_case
+      self.first_name = first_name.capitalize
+    end
 end
